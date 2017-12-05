c5cd08538e478b79b81a3083b6253d667e1ac1e5
tinkofee <34220049+tinkofee@users.noreply.github.com>
2017-12-05 23:26:44 +0200

    Webscrapped - Auchan and Carrefour
    Webscrapped Auchan and Carrefour and arranged em in a concise maneer.

--- Data preparation scripts/hipermarketuri.r
+++ Data preparation scripts/hipermarketuri.r
@@ -57,30 +57,52 @@ walk(lista_lanturi_retail$nume_lant,
 ###################################################################################
                                        #Auchan
 ###################################################################################
-date_Auchan<-read_html("data_html/auchan.html")
-date_Auchan1<-date_Auchan %>% 
-   html_nodes("text//javascript") %>%
-   map(xml_attrs) %>% 
-   map_df(~as.list(.))
+magazine_Auchan<-read_html("data_html/auchan.html") #Auchan HTML file contents
+###################################################################################
+#Auchan counties - Actually all the hypermarkets are present in the next JSON structure, so no need this one
+#                 If you don't need counties, delete this part of code.
+###################################################################################
+#judete_Auchan<-regmatches(html_text(date_Auchan),
+#           gregexpr("(?<=var countiesData = ).*?(?=];)", html_text(date_Auchan), perl = TRUE)
+#           )
+#judete_Auchan<-as.character(judete_Auchan)
+#judete_Auchan<-paste(judete_Auchan,"]")
+#judete_Auchan<-fromJSON(judete_Auchan)
+###################################################################################
+#Auchan actual hypermarkets
+###################################################################################
+magazine_Auchan<-regmatches(html_text(magazine_Auchan),
+                          gregexpr("(?<=var jsData = ).*?(?=];)", html_text(magazine_Auchan), perl = TRUE)
+)
+magazine_Auchan<-as.character(magazine_Auchan)
+magazine_Auchan<-paste(magazine_Auchan,"]")
+magazine_Auchan<-fromJSON(magazine_Auchan)
+###################################################################################
+                                    #Carrefour
+###################################################################################
+magazine_Carrefour<-read_html("data_html/carrefour.html") #Carrefour HTML file contents
+###################################################################################
+magazine_Carrefour<-regmatches(html_text(magazine_Carrefour),
+                            gregexpr("(?<=var coordonate_magazine = ).*?(?=];)", html_text(magazine_Carrefour), perl = TRUE)
+                              )
+magazine_Carrefour<-as.character(magazine_Carrefour)
+magazine_Carrefour<-paste(magazine_Carrefour,"]")
+magazine_Carrefour<-fromJSON(magazine_Carrefour)
 
-
-date_auchan2<-html_nodes(date_Auchan,"select")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ia din prima coloana numele localitatii si il adauga in coloana a 5-a
+for(i in 1:nrow(magazine_Carrefour)){ 
+   for(j in magazine_Carrefour[[i]]){
+      magazine_Carrefour[i,5]<-substr(magazine_Carrefour[[i]], 
+             which(strsplit(magazine_Carrefour[[i]],"")[[1]]=="/")[1]+1,
+             which(strsplit(magazine_Carrefour[[i]],"")[[1]]=="/")[2]-1
+      )
+   }
+   rm(i,j)
+}
+magazine_Carrefour<-as.data.frame(magazine_Carrefour)
+###################################################################################
+                                     #Cora
+###################################################################################
+magazine_Cora<-read_html("data_html/cora.html") #Cora HTML file contents
+###################################################################################
+#in progress, tomorrow is another day ^^
